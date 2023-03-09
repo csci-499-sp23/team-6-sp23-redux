7fdbc75a382c3a113376c2c18e6629fed2c7129a
Jordan Sukhnandan <kirazuto7@gmail.com>
2023-03-09 18:58:45 -0500

    main page added with nacbar and container

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import SwipeableContainer from './SwipeableContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar className="color-navbar" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img className="nav-logo" src="easyhangoutlogo.png"></img>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#preferences">Preferences</Nav.Link>
+                <Nav.Link href="#favorites">Favorites</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <img className="InfoButton" src=""></img>
+        <div className="HangoutContainer">
+          <SwipeableContainer></SwipeableContainer>
+        </div>
+        
       </header>
     </div>
   );
