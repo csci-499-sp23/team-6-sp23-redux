ec174abea68b9811523c5cad6bc63886e94ddc56
Jordan Sukhnandan <kirazuto7@gmail.com>
2023-03-09 19:02:43 -0500

    add homepage

--- my-app/src/SwipeableContainer.js
+++ my-app/src/SwipeableContainer.js
@@ -3,7 +3,7 @@ import './SwipeableContainer.css';
 function SwipeableContainer() {
     return(
         <div className="Container">
-            <img id="Container-Image" src="SwipeableContainerImage.jpeg"></img>
+            <img id="Container-Image" src="SwipeableContainerImage.jpeg" alt="hangout-image"></img>
             <div id="Container-Title">Brooklyn Bridge</div>
             <div id="Location">Location: Williamsburg Brooklyn</div>
             <div id="Distance">Distance: 5km away</div>
