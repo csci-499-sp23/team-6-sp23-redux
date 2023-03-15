9547c50715bcb91ad2ebb3681b244a8f5fe997bc
beaverseven <kamilpiziak@gmail.com>
2023-03-15 13:39:17 -0400

    LogIn Implemented

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,24 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-
-
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import SwipeableContainer from './SwipeableContainer';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from './containers/navbar'
+import HomePage from "./containers/homepage"
+import Login from './containers/Login';
+import SignUp from './containers/signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Navbar className="color-navbar" expand="lg">
-          <Container>
-            <Navbar.Brand href="#home">
-              <img className="nav-logo" src="easyhangoutlogo.png" alt="nav-logo"></img>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#preferences">Preferences</Nav.Link>
-                <Nav.Link href="#favorites">Favorites</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-
-        <img className="InfoButton" src="" alt="info-button"></img>
-        <div className="HangoutContainer">
-          <SwipeableContainer></SwipeableContainer>
-        </div>
+        <NavBar/>
+        <Router>
+          <Routes>
+            <Route exact path = "/Login" element = {<Login/>}/>
+            <Route exact path = "/homepage" element = {<HomePage/>}/>
+            <Route exact path = "/Signup" element = {<SignUp/>}/>
+            </Routes>
+        </Router>
         
-      </header>
+      
     </div>
   );
 }
