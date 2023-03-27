4634094aa007b495a9cb1029517d29384ad0304a
Chizy <chizy7@gmail.com>
2023-03-27 00:22:18 -0400

    fixes to auth path

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -16,7 +16,13 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const analytics = getAnalytics(app);
-export default app;
+const db = getFirestore(app);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
+
+export { app, db, auth, analytics };
+
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
+// export const analytics = getAnalytics(app);
+// export default app;
