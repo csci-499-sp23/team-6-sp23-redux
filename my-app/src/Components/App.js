26339dc8b4cf30c9e65f6e8eca418d6a3631f8bb
Chizy <32227554+chizy7@users.noreply.github.com>
2023-03-28 13:04:36 -0400

    Revert "Login and signup request"

--- my-app/src/Components/App.js
+++ my-app/src/Components/App.js
@@ -2,8 +2,6 @@ import '../Styles/App.css';
 import Favoritelist from './Favoritelist';
 import Homepage from './Homepage';
 import AppNavbar from './AppNavbar';
-import Login from './Login';
-import SignUp from './Signup';
 import Preferences from './Preferences';
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
@@ -18,8 +16,6 @@ function App() {
           <Route exact path="/" element={<Homepage/>}/>
           <Route exact path="/homepage" element={<Homepage/>}/>
           <Route exact path="/favorites" element={<Favoritelist/>}/>
-          <Route exact path = "/login" element = {<Login/>}/>
-          <Route exact path = "/signup" element = {<SignUp/>}/>
           <Route exact path="/preferences" element={<Preferences/>}/>
         </Routes>
       </Router>    
