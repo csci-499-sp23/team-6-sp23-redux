710510ee89ef51a3000d5523273eedd4af4af6d7
Chizy <32227554+chizy7@users.noreply.github.com>
2023-03-28 12:32:54 -0400

    Revert "Refactor Firebase/firestore"

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,23 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
  
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDIXJ5YT7hoNbBFqK3TBcV41-TzIO-7n7w",
+  authDomain: "fir-auth-6edd8.firebaseapp.com",
+  projectId: "fir-auth-6edd8",
+  storageBucket: "fir-auth-6edd8.appspot.com",
+  messagingSenderId: "904760319835",
+  appId: "1:904760319835:web:44fd0d957f114b4e51447e",
+  measurementId: "G-Q4TYKH9GG7",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
-const auth = getAuth(app);
-const analytics = getAnalytics(app);
-
-export { app, db, auth, analytics };
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+export default app;
