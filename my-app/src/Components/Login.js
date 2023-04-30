5e10318f317501e3a61e32fe262e1d4c75eb7ce2
Chizy <chizy7@gmail.com>
2023-04-29 22:51:59 -0400

    Login/signup styled

--- my-app/src/Components/Login.js
+++ my-app/src/Components/Login.js
@@ -82,7 +82,15 @@ const Login = (props) => {
 
   return (
     <>
-      <main className={LoginCSS.login}>
+      <main 
+        className={LoginCSS.login}
+        style={{
+          backgroundImage: `url(${process.env.PUBLIC_URL}/Images/background.jpg)`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat'
+        }}
+      >
         <section className={LoginCSS.LoginContainer}>
           <div className={LoginCSS.LoginDiv}>
             <form onSubmit={onLogin}> {/* Add onSubmit attribute */}
