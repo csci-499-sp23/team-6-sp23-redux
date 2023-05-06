2459dc711f258cbd912e1d83d28d35b902ec7735
JChen0915 <jackiechen0915@gmail.com>
2023-05-06 16:06:11 -0400

    Now limits the amount of locations queried based on number of categories

--- my-app/src/Services/HangoutService.js
+++ my-app/src/Services/HangoutService.js
@@ -5,18 +5,35 @@ import axios from 'axios';
 export async function getHangouts(location, categories, radius) {
     try{
 
+        let limit = 15;
+        const numCategories = Object.keys(categories).length;
+        if (numCategories >= 13) {
+            limit = 3; 
+        } 
+        else if (numCategories > 10) { 
+            limit = 4; 
+        } 
+        else if (numCategories > 8)
+        {
+            limit = 6;
+        }
+        else if  (numCategories > 5) { 
+            limit = 8; 
+         } 
+
+
         categories = categories.filter(category => category !== "");
         const hangoutsPromises = categories.map(async (category) => {
         try{
             if (radius > 0) {
-                //`http://localhost:3001/api/hangouts/${category}/${location}/${radius}` for testing
-               const promise = await axios.get(`https://us-central1-easy-hangout-68597.cloudfunctions.net/onHangouts/api/hangouts/${category}/${location}/${radius}`);
+                //`http://localhost:3001/api/hangouts/${category}/${location}/${radius}/${limit}` for testing
+               const promise = await axios.get(`https://us-central1-easy-hangout-68597.cloudfunctions.net/onHangouts/api/hangouts/${category}/${location}/${radius}/${limit}`);
                return promise.data.businesses.map(business => ({...business, ...{"category":`${category}`} }) );
             }
             else
             {
-                //`http://localhost:3001/api/hangouts/${category}/${location}/` for testing
-                const promise = await axios.get(`https://us-central1-easy-hangout-68597.cloudfunctions.net/onHangouts/api/hangouts/${category}/${location}/`);
+                //`http://localhost:3001/api/hangouts/${category}/${location}/${limit}` for testing
+                const promise = await axios.get(`https://us-central1-easy-hangout-68597.cloudfunctions.net/onHangouts/api/hangouts/${category}/${location}/${limit}`);
                 return promise.data.businesses.map(business => ({...business, ...{"category":`${category}`} }) );
             }
            
